629c16adf3fa166260bf81338f57f78c41ef9a55
Gurunath Solanki <gurunathsolanki@gmail.com>
2022-10-20 17:12:17 +0100

    Added logic to make a REST call using async function style.

--- src/App.js
+++ src/App.js
@@ -28,10 +28,28 @@ function App() {
       });
   };
 
+    async function fetchMoviesHandlerAsync() {
+      setIsLoading(true);
+      const response = await fetch("https://swapi.dev/api/films");
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((movie) => {
+        return {
+          id: movie.episode_id,
+          title: movie.title,
+          openingText: movie.opening_crawl,
+          releaseDate: movie.release_date,
+        };
+      });
+
+      setMovies(transformedMovies);
+      setIsLoading(false);
+    }
+
   return (
     <React.Fragment>
       <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+        <button onClick={fetchMoviesHandlerAsync}>Fetch Movies</button>
       </section>
       {movies.length > 0 && (
         <section>
